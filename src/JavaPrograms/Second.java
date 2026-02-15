a3dced09918751dc8dc04bfe982c997f3dfc544f
abhishekshahab <abhishekdasondhi1842003@gmailcom>
2026-02-15 13:12:34 +0530

    second programm added

--- src/JavaPrograms/Second.java
+++ src/JavaPrograms/Second.java
@@ -1,4 +1,15 @@
 package JavaPrograms;
+import java.util.Scanner;
 
 public class Second {
+      public static void main(String[] args){
+          Scanner sc= new Scanner(System.in);
+          System.out.print("Please enter your name: ");
+          String name= sc.nextLine();
+
+          System.out.printf("Welcome %s to KG Coding", name);
+
+
+      }
+
 }
