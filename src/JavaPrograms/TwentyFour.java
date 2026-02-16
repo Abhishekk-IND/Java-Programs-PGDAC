f6a656b9d6a01b32d6e9cc5cd5699b8f8a825e5f
abhishekshahab <abhishekdasondhi1842003@gmailcom>
2026-02-16 14:51:02 +0530

    TwentyThree programm added

---
+++ src/JavaPrograms/TwentyFour.java
@@ -0,0 +1,15 @@
+package JavaPrograms;
+import org.w3c.dom.ls.LSOutput;
+
+public class TwentyFour {
+
+    public static void main(String[] args){
+        System.out.println("welcome to print right half pyramid");
+        for (int i =1; i<=5; i++){
+            for (int j =1 ;j <=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
